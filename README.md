217842b09bf6593b17f76da5cfb1a2612fb0c8c4
zzq323 <zzq32323@gmail.com>
2023-12-14 13:05:41 +0800

    12.14

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Code-snippets
+
+
+
+
+$$
+\left\{ 
+\begin{array}{c}
+	f_5\cos 45\degree+f_6=f_9\cos 45\degree+f_{10}\\
+	f_5\sin 45\degree+f_7+f_9\sin 45\degree=15\\
+	f_1=f_6\\
+	f_3=10\\
+	f_1\cos 45\degree=f_4+f_5\cos 45\degree\\
+	f_1\sin 45\degree+f_3+f_5\sin 45\degree=0\\
+	f_4=f_8\\
+	f_7=0\\
+	f_5\cos 45\degree+f_6=f_9\cos 45\degree+f_10\\
+	f_5\sin 45\degree+f_7+f_9\sin 45\degree=15\\
+	f_{10}=f_{13}\\
+	{f_1}_1=20\\
+	f_8+f_9\cos 45\degree={f_1}_2\cos 45\degree\\
+	{f_1}_3+{f_1}_2\sin 45\degree=0\\
+\end{array} 
+\right. 
+$$
+
+
+给定一个序列，序列编号从 1 到 N 。每个编号上都有一个值，代表该编号的颜色。编号的颜色总共有 K 种，编号的颜色从 1 到 K 。第 i 个编号的颜色为 Ai。Fi 表示恰好有 i 种颜色的子串数量。请计算$(\sum_{i=1}^n*(F_i * 131^i)) mod (10^9 + 7)$
+
+注意：mod 代表取模； 子串是序列中的任意一段连续区间
--- main.cpp
+++ main.cpp
@@ -1,1 +1,210 @@
-//HuffmanTree
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+
+using namespace std;
+
+const int N=110;
+int M[N][N];
+
+int n,m;
+bool flag;
+
+void print();
+
+void UP(int x,int y)
+{  
+    auto clean=[](int col)-> void {
+        int l=1,r=1;
+        while(true){
+            while(r<=n && M[r][col])++r;
+            l=r;
+            while(l<=n && !M[l][col])++l;
+            if(l==n+1 || r==n+1)break;
+            swap(M[l][col],M[r][col]);
+        }
+    };
+
+    for(int i=1;i<=n;++i){
+        clean(i);
+        for(int j=1;j<n;++j){
+            if(M[j][i]==M[j+1][i]){
+                M[j][i]+=M[j+1][i];
+                M[j+1][i]=0;
+                if(M[j][i]==2048){
+                    flag=1;
+                }
+            }
+        }
+        clean(i);
+    }
+
+    if(!M[x][y])
+        M[x][y]=2;
+    
+}
+
+void DOWN(int x,int y)
+{
+    auto clean=[](int col)-> void {
+        int l=n,r=n;
+        while(true){
+            while(r && M[r][col])--r;
+            l=r;
+            while(l && !M[l][col])--l;
+            if( !l ||  !r)break;
+            swap(M[l][col],M[r][col]);
+        }
+    };
+
+    for(int i=1;i<=n;++i){
+        clean(i);
+        for(int j=n-1;j;--j){
+            if(M[j+1][i]==M[j][i]){
+                M[j+1][i]+=M[j][i];
+                M[j][i]=0;
+                if(M[j+1][i]==2048){
+                    flag=1;
+                }
+            }
+        }
+        clean(i);
+    }
+
+    if(!M[x][y])
+        M[x][y]=2;
+}
+
+void LEFT(int x,int y)
+{
+    auto clean=[](int row)-> void {
+        int l=1,r=1;
+        while(true){
+            while(r<=n && M[row][r])++r;
+            l=r;
+            while(l<=n && !M[row][l])++l;
+            if( l==n+1 ||  r==n+1)break;
+            swap(M[row][l],M[row][r]);
+        }
+    };
+
+    for(int i=1;i<=n;++i){
+        clean(i);
+        for(int j=1;j<n;++j){
+            if(M[i][j]==M[i][j+1]){
+                M[i][j]+=M[i][j+1];
+                M[i][j+1]=0;
+                if( M[i][j]==2048 ){
+                    flag=1;
+                }
+            }
+        }
+        clean(i);
+    }
+
+    if(!M[x][y])
+        M[x][y]=2;
+}
+
+void RIGHT(int x,int y)
+{
+    auto clean=[](int row)-> void {
+        int l=n,r=n;
+        while(true){
+            while(r && M[row][r])--r;
+            l=r;
+            while(l && !M[row][l])--l;
+            if( !l || !r)break;
+            swap(M[row][l],M[row][r]);
+        }
+    };
+
+    for(int i=1;i<=n;++i){
+        clean(i);
+        for(int j=n-1;j;--j){
+            if(M[i][j+1]==M[i][j]){
+                M[i][j+1]+=M[i][j];
+                M[i][j]=0;
+                if(M[i][j+1]==2048){
+                    flag=1;
+                }
+            }
+        }
+        clean(i);
+    }
+
+    if(!M[x][y])
+        M[x][y]=2;
+}
+
+void print()
+{
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=n;++j){
+            printf(" %d"+(j==1),M[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    scanf("%d%d",&n,&m);
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=n;++j){
+            scanf("%d",M[i]+j);
+        }
+    }
+
+    while(m--){
+        char op;
+        int x,y;
+        scanf("\n%c",&op);
+        scanf("%d%d",&x,&y);
+        // printf("%c %d %d\n",op,x,y);
+
+        switch (op){
+        case 'U':
+            UP(x,y);
+            break;
+        case 'D':
+            DOWN(x,y);
+            break;
+        case 'L':
+            LEFT(x,y);
+            break;
+        case 'R':
+            RIGHT(x,y);
+            break;
+        }
+
+        // puts("-------------------");
+        // print();
+        // puts("-------------------");
+
+        if(flag){
+            puts("Yes");
+            print();
+            break;
+        }
+    }
+
+    if(!flag){
+        puts("No");
+        print();
+    }
+
+    return 0;
+}
+
+
+/*
+4 100
+0 0 2 4
+4 4 8 4
+8 8 2 4
+2 2 2 2
+U 4 2
+D 1 1
+L 3 3
+*/
